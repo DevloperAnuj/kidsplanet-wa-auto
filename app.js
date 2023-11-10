8275b4ef8d27b018ed1f4b20bc1e7bede72a387a
DevloperAnuj <dev.anujkamble+github.com>
2023-11-11 00:49:47 +0530

    Add Loggers

--- app.js
+++ app.js
@@ -1,21 +1,67 @@
 require("dotenv").config();
+const fs = require("fs");
+const path = require("path");
 const express = require("express");
 const app = express();
 const request = require("request");
 const morgan = require("morgan");
-const port = process.env.PORT;
+const createCsvWriter = require("csv-writer").createObjectCsvWriter;
 
-app.use(morgan("dev"));
+const port = process.env.PORT;
+const csvWriter = createCsvWriter({
+  path: __dirname + "/file.csv",
+  header: [
+    { id: "name", title: "NAME" },
+    { id: "number", title: "NUMBER" },
+    { id: "date", title: "DATELOG" },
+  ],
+});
+
+let assessLogStream = fs.createWriteStream(path.join(__dirname, "access.log"), {
+  flags: "a",
+});
+
+app.use(morgan("combined"));
+app.use(morgan("combined", { stream: assessLogStream }));
 app.use(express.json());
 
 app.get("/", (req, res) => {
   console.log("Its Get Request");
-  res.json({
+  res.status(200).json({
     success: true,
   });
 });
 
-app.post("/wsp", (req, res, next) => {
+app.get("/log", (req, res) => {
+  filePath = path.join(__dirname, "access.log");
+  fs.readFile(filePath, { encoding: "utf-8" }, function (err, data) {
+    if (!err) {
+      console.log("received data: " + data);
+      res.writeHead(200, { "Content-Type": "text/plain" });
+      res.write(data);
+      res.end();
+    } else {
+      console.log(err);
+    }
+  });
+});
+
+app.get("/user", (req, res) => {
+  filePath = path.join(__dirname, "file.csv");
+  fs.readFile(filePath, { encoding: "utf-8" }, function (err, data) {
+    if (!err) {
+      console.log("received data: " + data);
+      res.writeHead(200, { "Content-Type": "text/plain" });
+      res.write(data);
+      res.end();
+    } else {
+      console.log(err);
+    }
+  });
+});
+
+app.post("/wsp", async (req, res, next) => {
+  let dateTime = new Date();
   const membership =
     req.body["user_meta"]["pmpro_CardType"][0] === "" ? "Free" : "Premium";
   setBody = {
@@ -49,6 +95,13 @@ app.post("/wsp", (req, res, next) => {
       ],
     },
   };
+  const record = [
+    {
+      name: req.body["user_meta"]["nickname"][0],
+      number: `91${req.body["user_meta"]["phone"][0]}`,
+      date: `${dateTime}`,
+    },
+  ];
 
   // console.log(req.body["user_meta"]);
   // console.log("=======================================");
@@ -57,8 +110,10 @@ app.post("/wsp", (req, res, next) => {
   // console.log(req.body["user_meta"]["phone"][0]);
   // console.log(membership);
 
+  let finalRespObj = {};
+
   try {
-    request.post(
+    await request.post(
       {
         headers: {
           "Content-Type": "application/json",
@@ -69,17 +124,23 @@ app.post("/wsp", (req, res, next) => {
       },
       (err, resp, body) => {
         if (!err && resp.statusCode === 200) {
-          console.log({ success: true });
+          console.log({ postBody: setBody, response: body });
+          finalRespObj = { postBody: setBody, response: body };
         } else {
-          console.log(body);
           console.log(err);
+          finalRespObj = { postBody: setBody, error: err };
         }
       }
     );
-    res.status(201).json({ success: true });
+    await csvWriter
+      .writeRecords(record) // returns a promise
+      .then(() => {
+        console.log("...Done Writing In CSV" + record);
+      });
+    res.status(201).json(finalRespObj);
   } catch (error) {
     console.log(error);
-    res.status(407).json({ err: error });
+    res.status(407).json(finalRespObj);
   }
   next();
 });
