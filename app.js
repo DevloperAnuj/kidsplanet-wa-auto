e0da724f32c5a1841eebeff5d35af4e37752987a
DevloperAnuj <dev.anujkamble+github.com>
2023-12-24 15:22:31 +0530

    Fix 3 months days

--- app.js
+++ app.js
@@ -140,7 +140,7 @@ app.post("/submit", async (req, res, next) => {
   var subendDate = null;
   var subName = null;
   if (req.body["form_id"] === "df748f4") {
-    subendDate = addDays(30);
+    subendDate = addDays(90);
     subName = "3 Months";
   } else if (req.body["form_id"] === "bbd2f7b") {
     subendDate = addDays(180);
